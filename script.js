1fc3eb07658664e656d8614d4b4510526110b5a2
Jo El <joelag1235@gmail.com>
2024-02-22 23:56:31 +0700

    .js file ready

--- script.js
+++ script.js
@@ -1,93 +1,92 @@
- const transactionEl = document.querySelector('.transactions');
- const numincomeEl = document.querySelector('.number--income');
- const numexpensesEl = document.querySelector('.number--expenses');
- const balancenumEl = document.querySelector('.balance-number');
- const formEl = document.querySelector('.form');
- const inputDescriptionEl = document.querySelector('.input--description');
- const inputAmountEl = document.querySelector('.input--amount');
+const transactionsEl = document.querySelector('.transactions');
+const balanceNumberEl = document.querySelector('.balance-number');
+const numberIncomeEl = document.querySelector('.number--income');
+const numberExpensesEl = document.querySelector('.number--expenses');
+const formEl = document.querySelector('.form');
+const inputDescriptionEl = document.querySelector('.input--description');
+const inputAmountEl = document.querySelector('.input--amount');
 
+const submitHandler = event => {
+  // prevent default behavior
+  event.preventDefault();
+  
+  // get input values
+  const description = inputDescriptionEl.value;
+  const amount = +inputAmountEl.value;
+  
+  // create transaction item HTML
+  const transactionItemHTML = `
+    <li class="transaction transaction--${amount > 0 ? 'income' : 'expense'}">
+      <span class="transaction__text">${description}</span>
+      <span class="transaction__amount">${amount > 0 ? '+' : ''}${amount}</span>
+      <button class="transaction__btn">X</button>
+    </li>
+  `;
+  
+  // insert new HTML
+  transactionsEl.insertAdjacentHTML('beforeend', transactionItemHTML);
+  
+  // clear form inputs
+  inputDescriptionEl.value = '';
+  inputAmountEl.value = '';
+  
+  // unfocus (blur) form inputs
+  inputDescriptionEl.blur();
+  inputAmountEl.blur();
+  
+  // update income or expenses
+  if (amount > 0) {
+    const currentIncome = +numberIncomeEl.textContent;
+    const updatedIncome = currentIncome + amount;
+    numberIncomeEl.textContent = updatedIncome;
+  } else {
+    const currentExpenses = +numberExpensesEl.textContent;
+    const updatedExpenses = currentExpenses + amount * -1;
+    numberExpensesEl.textContent = updatedExpenses;
+  }
+  
+  // update balance
+  const income = +numberIncomeEl.textContent;
+  const expenses = +numberExpensesEl.textContent;
+  const updatedBalance = income - expenses;
+  balanceNumberEl.textContent = updatedBalance;
+  
+  // make red if balance negative
+  if (income - expenses < 0) {
+    balanceNumberEl.style.color = 'red';
+  }
+}
 
+formEl.addEventListener('submit', submitHandler);
 
-// submit event listener
- formEl.addEventListener('submit', function(event){
-    // removing  render default annoying
-    event.preventDefault();
-    const description = inputDescriptionEl.value;
-    const amount = inputAmountEl.value;
+const clickHandler = (event) => {
+  // remove transaction item visually
+  const clickedEl = event.target.parentNode;
+  clickedEl.remove();
+  
+  // update income or expenses
+  const amountEl = clickedEl.querySelector('.transaction__amount');
+  const amount = +amountEl.textContent;
+  
+  if (amount > 0) {
+    const currentIncome = +numberIncomeEl.textContent;
+    const updatedIncome = currentIncome - amount;
+    numberIncomeEl.textContent = updatedIncome;
+  } else {
+    const currentExpenses = +numberExpensesEl.textContent;
+    const updatedExpenses = currentExpenses - amount * -1;
+    numberExpensesEl.textContent = updatedExpenses;
+  }
+  
+  // update balance
+  const income = +numberIncomeEl.textContent;
+  const expenses = +numberExpensesEl.textContent;
+  balanceNumberEl.textContent = income - expenses;
+  
+  // make red if balance negative
+  if (income - expenses < 0) {
+    balanceNumberEl.style.color = 'red';
+  }
+}
 
-
-
-    const transactionItemHtml = `
-        <li class="transaction transaction--income">
-            <span class="transaction__text">${description}</span>
-            <span class="transaction__amount">+${amount}</span>
-            <button class="transaction__btn">X</button>
-        </li> `;
-
-        // inputting in one by one last in format
-        transactionEl.insertAdjacentHTML('beforeend', transactionItemHtml);
-
-        //clear form inputs
-        inputDescriptionEl.value = '';
-        inputAmountEl.value = '';
-
-        //blur from inputs
-        inputDescriptionEl.blur();
-        inputAmountEl.blur();
-        
-
-        //issue here!!!!!!!!!!!
-
-        // if (amount > 0){
-        //     const current_income = numincomeEl.textContent;
-        //     const updated_income = current_income + amount;
-        //     numincomeEl.textContent = updated_income;
-        //     console.log(numincomeEl)
-        // }else{
-        //     const current_expense = numexpensesEl.textContent;
-        //     const updated_expense = current_expense + (amount) * -1;
-        //     numexpensesEl.textContent = updated_expense;
-        //     // console.log("Negative number");
-        // }
-
-//  });
-
-
-    const clickHandler = (event) => {
-        const clickedEl = event.target.parentNode
-        // console.log(clickedEl)
-        clickedEl.remove();
-
-        //update income and expenses information
-        const amountEl = clickedEl.querySelector('.transaction__amount')
-        const amount = +amountEl.textContent;
-
-        if (amount > 0){
-            const current_income = +numincomeEl.textContent;
-            const updated_income = current_income - amount;
-            numincomeEl.textContent = updated_income;
-        }else{
-            const current_expense = +numexpensesEl.textContent;
-            const updated_expense = current_expense - (amount) * -1;
-            numexpensesEl.textContent = updated_expense;
-            // console.log("Negative number");
-        }
-        //update balance
-            const incomes = +numincomeEl.textContent;
-            const expenses = +numexpensesEl.textContent;
-            balancenumEl.textContent = incomes - expenses;
-            // console.log(balancenumEl)
-
-            balancenumEl.style.color = incomes - expenses < 0 ? 'red' : '';
-    }
-    //  remove transaction list one by one
-
-    transactionEl.addEventListener('click', clickHandler);
-
-
-
-
-
-
-
-
+transactionsEl.addEventListener('click', clickHandler);
